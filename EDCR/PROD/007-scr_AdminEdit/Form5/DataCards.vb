a149494b86b035051074fb6f7d70df07eaaf7021
shaikhintekhabindia <shaikhintekhabindia@gmail.com>
2026-02-05 18:00:39 +0530

    05FebChanges

---
+++ EDCR/PROD/007-scr_AdminEdit/Form5/DataCards.vb
@@ -0,0 +1,56 @@
+EDCRNumber DataCard1
+Default - VarItem.EDCRNumber
+Width - 400
+
+Description_DataCard1
+Default - VarItem.Description
+Width - 400
+
+Justification_DataCard1
+Default - VarItem.Justification
+Width - 400
+
+RequestDate_DataCard2
+Default - VarItem.RequestDate
+Width - 400
+
+ReviewByDate_DataCard2
+Default - VarItem.ReviewByDate
+Width - 400
+
+RequestedBy_DataCard2
+Default - VarItem.RequestedBy
+Width - 400
+
+Reviewing Manager_DataCard3
+Default - VarItem.ReviewingManager
+Width - 400
+
+FormStatus_DataCard1
+Default - VarItem.FormStatus
+Width - 400
+
+RequestType_DataCard1
+Default - VarItem.RequestType
+Width - 400
+
+EngLocation_DataCard2
+Default - VarItem.EngLocation
+Width - 400
+
+ProductLine_DataCard1
+Default - VarItem.ProductLine
+Width - 400
+
+ProductLineMgr_DataCard2
+Default - VarItem.ProductLineMgr
+Update - DataCardValue23_1.Value
+Width - 400
+
+DocumentNumber_DataCard1
+Default - VarItem.DocumentNumber
+DisplayMode - DisplayMode.Edit
+Width - 400
+
+Attachments_DataCard1
+Width - 400
